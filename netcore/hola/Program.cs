bed0105013ddc5a7755a886cc8bf87f438090c15
Víctor Velarde <victor.velarde@gmail.com>
2016-07-20 22:20:45 +0200

    Hola mundo c# - VSCode ubuntu

---
+++ netcore/hola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        { 
+            Console.WriteLine("Hola << dotnet run << dotnet restore << dotnet new");
+        }
+    }
+}
